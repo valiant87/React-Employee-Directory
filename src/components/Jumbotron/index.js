58ec27090cfd35c99f0e28982e2cf908f4cb8024
Val Nimirenco <valnimirenco@gmail.com>
2021-02-22 21:22:20 -0800

    added jumbotron

---
+++ src/components/Jumbotron/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Jumbotron() {
+  return (
+    <div className="jumbotron">
+      <h1>React Employee Directory</h1>
+      <p>Click something</p>
+    </div>
+  );
+}
+
+export default Jumbotron;
